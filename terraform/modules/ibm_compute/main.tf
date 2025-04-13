5677278122965d9d8369e4a7d01eb9000753afd7
Vivek Raj <vivek.raj@talentica.com>
2025-04-13 15:34:03 +0530

    Added configuration for IBM cloud vsi

--- terraform/modules/ibm_compute/main.tf
+++ terraform/modules/ibm_compute/main.tf
@@ -1,1 +1,13 @@
-
+resource "ibm_is_instance" "vsi" {
+  name              = "social-vsi"
+  image             = var.image_id
+  profile           = var.profile
+  zone              = var.zone1
+  vpc               = ibm_is_vpc.vpc.id
+  primary_network_interface {
+    subnet          = ibm_is_subnet.public_subnet.id
+    security_groups = [ibm_is_security_group.vsi_sg.id]
+  }
+  keys              = [var.ssh_key_id]
+  resource_group    = var.resource_group
+}
